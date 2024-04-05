d642efe94f9b7e3a7899aa931383a86fc04bb759
Jur de Vries <jur.devries@simplicate.nl>
2024-04-05 16:12:19 +0200

    Fixed pre-commit problems

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,1 @@
-# -*- coding: utf-8 -*-
-
 from . import food_product
--- models/food_product.py
+++ models/food_product.py
@@ -1,8 +1,8 @@
-from odoo import models, fields
+from odoo import fields, models
 
 
 class FoodProduct(models.Model):
-    _inherit = 'product.template'
+    _inherit = "product.template"
 
-    plu_code = fields.Integer(string='Plu code', required=False)
+    plu_code = fields.Integer(string="Plu code", required=False)
     ingredients = fields.Text(help="Beschrijving van de ingredienten.")
